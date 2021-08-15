ccaf5a7b4b27388e525a319b16ad2419f8e92c91
sha <krkblizz@hotmail.fr>
2021-08-15 17:08:34 +0200

    Add a select with the possibility to change the minimum time of a meeting (15 mn, 30mn or 1h)

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,15 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import Container from '@material-ui/core/Container';
 import 'react-big-calendar/lib/css/react-big-calendar.css'
 import { Calendar, View, DateLocalizer } from 'react-big-calendar'
 import { momentLocalizer } from 'react-big-calendar';
 import moment from 'moment'
+import InputLabel from '@material-ui/core/InputLabel';
+import MenuItem from '@material-ui/core/MenuItem';
+import FormHelperText from '@material-ui/core/FormHelperText';
+import FormControl from '@material-ui/core/FormControl';
+import Select from '@material-ui/core/Select';
 import 'moment/locale/fr';
 
 const localizer = momentLocalizer(moment);
@@ -31,38 +36,59 @@ class CalendarEvent {
 const App: React.FC = () => {
 
   const [events, setEvents] = useState([] as CalendarEvent[]);
+  const [step, setStep] = useState(30);
 
+  const handleChangeStep = (event: any) => {
+      setStep(Number(event.target.value));
+  };
 
-
-  const handleSelect = ({ start, end } : { start: any; end: any}) => {
+  const handleSelect = ({ start, end }: { start: any; end: any }) => {
     const title = window.prompt('New Event name')
 
     if (title) {
-        let newEvent = {} as CalendarEvent;
-        newEvent.start = moment(start).toDate();
-        newEvent.end = moment(end).toDate();
-        newEvent.title = title;
-        setEvents([
-          ...events,
-          newEvent
-        ])
+      let newEvent = {} as CalendarEvent;
+      newEvent.start = moment(start).toDate();
+      newEvent.end = moment(end).toDate();
+      newEvent.title = title;
+      setEvents([
+        ...events,
+        newEvent
+      ])
     }
   }
 
   return (
     <div className="App">
+      <FormControl >
+        <InputLabel >
+          Step
+        </InputLabel>
+        <Select
+          labelId="simple-select-placeholder-label"
+          id="simple-select"
+          value={step}
+          onChange={handleChangeStep}
+          displayEmpty
+        >
+          <MenuItem value={15}>15 mn</MenuItem>
+          <MenuItem value={30}>30 mn</MenuItem>
+          <MenuItem value={60}>1 h</MenuItem>
+        </Select>
+        <FormHelperText>Durée minimum d'une réunion</FormHelperText>
+      </FormControl>
       <Container maxWidth="md">
         <Calendar
           selectable
           localizer={localizer}
           events={events}
+          views={['week']}
           defaultView={allViews[0]}
           scrollToTime={new Date(1970, 1, 1, 6)}
           defaultDate={new Date(2015, 3, 12)}
           onSelectEvent={event => alert(event.title)}
           onSelectSlot={handleSelect}
           culture={'fr'}
-          step={15}
+          step={step}
         />
       </Container>
     </div>
