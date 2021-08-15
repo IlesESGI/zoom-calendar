7e8fd6a95cc836ceffcb505da3cc43871b5072be
sha <krkblizz@hotmail.fr>
2021-08-15 20:58:48 +0200

    Limit hours to work hours (7AM to 9PM) + add modal to assign client name for the meeting + add toast to warn users when mistaks occur

--- src/App.tsx
+++ src/App.tsx
@@ -5,11 +5,18 @@ import 'react-big-calendar/lib/css/react-big-calendar.css'
 import { Calendar, View, DateLocalizer } from 'react-big-calendar'
 import { momentLocalizer } from 'react-big-calendar';
 import moment from 'moment'
-import InputLabel from '@material-ui/core/InputLabel';
+import Button from '@material-ui/core/Button';
+import TextField from '@material-ui/core/TextField';
+import Dialog from '@material-ui/core/Dialog';
+import DialogActions from '@material-ui/core/DialogActions';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogContentText from '@material-ui/core/DialogContentText';
+import DialogTitle from '@material-ui/core/DialogTitle';
 import MenuItem from '@material-ui/core/MenuItem';
 import FormHelperText from '@material-ui/core/FormHelperText';
 import FormControl from '@material-ui/core/FormControl';
 import Select from '@material-ui/core/Select';
+import Box from '@material-ui/core/Box';
 import 'moment/locale/fr';
 
 const localizer = momentLocalizer(moment);
@@ -37,19 +44,46 @@ const App: React.FC = () => {
 
   const [events, setEvents] = useState([] as CalendarEvent[]);
   const [step, setStep] = useState(30);
+  const [open, setOpen] = React.useState(false);
+  const [client, setClient] = useState('');
+
+  const messagesAltertBox = {
+    previousDate: "Impossible de positionner un créneau à cette heure car la date choisie est antérieure à la date actuelle !",
+    lunchTime: "Impossible de positionner un créneau à cette heure pendant la pause midi !",
+    concurentMeeting: "Impossible de positionner un créneau à cette heure car il y a deja un créneau de programmer à ette heure !"
+  }
+
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  const handleChangeClient = (event: any) => {
+    setClient(event.target.value);
+    console.log(client);
+  }
+
+  const today = new Date();
+  const minRangeHour = new Date(today.getFullYear(), today.getMonth(), today.getDate(), 7, 0, 0);
+  const maxRangeHour = new Date(today.getFullYear(), today.getMonth(), today.getDate(), 21, 0, 0);
 
   const handleChangeStep = (event: any) => {
-      setStep(Number(event.target.value));
+    setStep(Number(event.target.value));
   };
 
   const handleSelect = ({ start, end }: { start: any; end: any }) => {
-    const title = window.prompt('New Event name')
+    //handleClickOpen
 
-    if (title) {
+    setOpen(true);
+
+    if (client) {
       let newEvent = {} as CalendarEvent;
       newEvent.start = moment(start).toDate();
       newEvent.end = moment(end).toDate();
-      newEvent.title = title;
+      newEvent.title = client;
       setEvents([
         ...events,
         newEvent
@@ -59,23 +93,33 @@ const App: React.FC = () => {
 
   return (
     <div className="App">
-      <FormControl >
-        <InputLabel >
-          Step
-        </InputLabel>
-        <Select
-          labelId="simple-select-placeholder-label"
-          id="simple-select"
-          value={step}
-          onChange={handleChangeStep}
-          displayEmpty
-        >
-          <MenuItem value={15}>15 mn</MenuItem>
-          <MenuItem value={30}>30 mn</MenuItem>
-          <MenuItem value={60}>1 h</MenuItem>
-        </Select>
-        <FormHelperText>Durée minimum d'une réunion</FormHelperText>
-      </FormControl>
+      <div>
+        <Dialog open={open} onClose={handleClose} aria-labelledby="form-dialog-title">
+          <DialogTitle id="form-dialog-title">Création d'une réunion</DialogTitle>
+          <DialogContent>
+            <DialogContentText>
+              Nom du client avec qui la réunion aura lieu :
+            </DialogContentText>
+            <TextField
+              autoFocus
+              margin="dense"
+              id="client"
+              fullWidth
+              value={client}
+              onChange={handleChangeClient}
+            />
+          </DialogContent>
+          <DialogActions>
+            <Button onClick={handleClose} color="primary">
+              Cancel
+            </Button>
+            <Button onClick={handleClose} color="primary">
+              Subscribe
+            </Button>
+          </DialogActions>
+        </Dialog>
+      </div>
+      <Box m={2} />
       <Container maxWidth="md">
         <Calendar
           selectable
@@ -83,13 +127,28 @@ const App: React.FC = () => {
           events={events}
           views={['week']}
           defaultView={allViews[0]}
-          scrollToTime={new Date(1970, 1, 1, 6)}
-          defaultDate={new Date(2015, 3, 12)}
-          onSelectEvent={event => alert(event.title)}
+          defaultDate={today}
           onSelectSlot={handleSelect}
           culture={'fr'}
           step={step}
+          min={minRangeHour}
+          max={maxRangeHour}
         />
+        <Box m={2} />
+        <FormControl >
+          <Select
+            labelId="simple-select-placeholder-label"
+            id="simple-select"
+            value={step}
+            onChange={handleChangeStep}
+            displayEmpty
+          >
+            <MenuItem value={15}>15 mn</MenuItem>
+            <MenuItem value={30}>30 mn</MenuItem>
+            <MenuItem value={60}>1 h</MenuItem>
+          </Select>
+          <FormHelperText>Durée minimum d'une réunion</FormHelperText>
+        </FormControl>
       </Container>
     </div>
   );
