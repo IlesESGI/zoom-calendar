f8468869db52649e513e6a51a6d4523e3ed5947d
sha <krkblizz@hotmail.fr>
2021-08-15 16:28:53 +0200

    Add calendar lib and adapt it to French language

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,69 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
 import Container from '@material-ui/core/Container';
-import Typography from '@material-ui/core/Typography';
-import Box from '@material-ui/core/Box';
-import Link from '@material-ui/core/Link';
+import 'react-big-calendar/lib/css/react-big-calendar.css'
+import { Calendar, View, DateLocalizer } from 'react-big-calendar'
+import { momentLocalizer } from 'react-big-calendar';
+import moment from 'moment'
+import 'moment/locale/fr';
 
-function Copyright() {
-  return (
-    <Typography variant="body2" color="textSecondary" align="center">
-      {'Copyright © '}
-      <Link color="inherit" href="https://material-ui.com/">
-        Your Website
-      </Link>{' '}
-      {new Date().getFullYear()}
-      {'.'}
-    </Typography>
-  );
+const localizer = momentLocalizer(moment);
+
+const allViews: View[] = ['week'];
+
+interface Props {
+  localizer: DateLocalizer;
+}
+
+class CalendarEvent {
+  title: string;
+  start: Date;
+  end: Date;
+  tooltip?: string;
+
+  constructor(_title: string, _start: Date, _endDate: Date) {
+    this.title = _title;
+    this.start = _start;
+    this.end = _endDate;
+  }
 }
 
 const App: React.FC = () => {
+
+  const [events, setEvents] = useState([] as CalendarEvent[]);
+
+
+
+  const handleSelect = ({ start, end } : { start: any; end: any}) => {
+    const title = window.prompt('New Event name')
+
+    if (title) {
+        let newEvent = {} as CalendarEvent;
+        newEvent.start = moment(start).toDate();
+        newEvent.end = moment(end).toDate();
+        newEvent.title = title;
+        setEvents([
+          ...events,
+          newEvent
+        ])
+    }
+  }
+
   return (
     <div className="App">
-      <Container maxWidth="sm">
-        <Box my={4}>
-          <Typography variant="h4" component="h1" gutterBottom>
-            Create React App v4-beta example
-          </Typography>
-          <Copyright />
-        </Box>
+      <Container maxWidth="md">
+        <Calendar
+          selectable
+          localizer={localizer}
+          events={events}
+          defaultView={allViews[0]}
+          scrollToTime={new Date(1970, 1, 1, 6)}
+          defaultDate={new Date(2015, 3, 12)}
+          onSelectEvent={event => alert(event.title)}
+          onSelectSlot={handleSelect}
+          culture={'fr'}
+          step={15}
+        />
       </Container>
     </div>
   );
